3209d76d9e976b32eb30b87df260434b4b51a3fc
David Castelblanco Benavides <cbdavides@gmail.com>
2017-10-12 22:49:58 -0500

    Added form to create an expense, with a basic functionality

---
+++ sales/static/js/gasto.js
@@ -0,0 +1,68 @@
+$(document).ready(function() {
+
+    elements = {
+        'price':            $('#price'),
+        'descripcion':      $('#descripcion'),
+        'cedula_vendedor':  $('#cedula_vendedor')
+    }
+
+    forms = {
+        'register':     $('#register'),
+    }
+
+    initialize(elements)
+
+    forms['register'].on('submit', function(e) {
+
+        var price           =   to_int(elements['price']),
+            description     =   trimText(elements['descripcion']),
+            owner_document  =   trimText(elements['cedula_vendedor'])
+
+        if(!price || price <= 0) {
+
+            alert('Ingresa un precio válido')
+            elements['price'].val('')
+            elements['price'].focus()
+
+        } else {
+            $.post('/gasto/', {
+                value : price,
+                description:description,
+                owner_document: owner_document
+            }, function(data) {
+
+                if(data.ok) {
+                    alert('La salida se registro correctamente.')
+                    initialize(elements)
+                    
+                } else {
+                    alert(data.msg)
+                }
+
+            })
+            
+        }
+
+        e.preventDefault()
+    }) 
+
+
+})
+
+function to_int($element) {
+    var value = $element.val()
+    value = value == '' ? 0 : parseInt(value)
+    return value
+}
+
+function trimText($element) {
+    return $.trim($element.val())
+}
+
+function initialize(elements) {
+
+    elements['price'].val('')
+    elements['descripcion'].val('')
+    elements['cedula_vendedor'].val('')
+
+}
