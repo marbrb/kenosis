8d06ec7051bbf9677a463fefcf91b3bdb22ed590
David Castelblanco Benavides <cbdavides@gmail.com>
2017-10-19 16:20:01 -0500

    Service sales and product sales are two different things now.

--- sales/static/js/product.js
+++ sales/static/js/product.js
@@ -3,21 +3,15 @@ $(document).ready(function() {
     elements = {
         'name':             $('#name'),
         'price':            $('#price'),
-        'amount':           $('#amount'),
         'bar_code':         $('#bar_code'),
         'comission':        $('#comission'),
-        'descripcion':      $('#descripcion'),
-        'tipo_ingreso':     $('#tipo_ingreso'),
         'pay_with_card':    $('#pay_with_card'),
-        'amount_holder':    $('#amount_holder'),
         'cedula_cliente':   $('#cedula_cliente'),
         'cedula_vendedor':  $('#cedula_vendedor')
     }
 
     buttons = {
-        'add_product':      $('#add_product'),
         'search_product':   $('#search_product'),
-        'remove_product':   $('#remove_product'),
         'enviar':           $('#enviar')
     }
 
@@ -27,36 +21,6 @@ $(document).ready(function() {
     }
 
     initialize(elements)
-    elements['tipo_ingreso'].val('Producto')
-
-    elements['tipo_ingreso'].change(function() {
-        $tipo = elements['tipo_ingreso']
-        initialize(elements)
-
-        if($tipo.val() === 'Producto')
-            saleProduct(elements, buttons)
-
-        else
-            saleService(elements, buttons)
-
-    })
-
-    buttons['add_product'].on('click', function() {
-
-        var maximum_value = to_int(elements['amount_holder'])
-
-        var value = to_int(elements['amount']) + 1
-        value = (value > maximum_value) ? maximum_value : value
-        elements['amount'].val(value)
-
-    })
-
-    buttons['remove_product'].on('click', function() {
-
-        var value = to_int(elements['amount']) - 1
-        elements['amount'].val(value < 0 ? 0 : value)
-
-    })
 
     forms['get_product'].on('submit', function(e) {
         var codigo = $.trim(elements['bar_code'].val())
@@ -74,8 +38,6 @@ $(document).ready(function() {
 
                     elements['name'].val(data.name)
                     elements['price'].val(data.price)
-                    elements['amount_holder'].val(data.amount)
-
                 }
 
             })
@@ -90,53 +52,33 @@ $(document).ready(function() {
 
         var name            =   trimText(elements['name']),
             price           =   to_int(elements['price']),
-            amount          =   to_int(elements['amount']),
             is_card         =   elements['pay_with_card'].prop('checked'),
             bar_code        =   trimText(elements['bar_code'])
             comission       =   to_int(elements['comission']),
-            description     =   trimText(elements['descripcion']),
-            tipo_ingreso    =   elements['tipo_ingreso'].val(),
             owner_document  =   trimText(elements['cedula_vendedor']),
             client_document =   trimText(elements['cedula_cliente'])
-
-
-        if(amount < 1) {
-            alert('La cantidad minima es 1')
-            buttons['enviar'].attr('disabled', false)
-
-        } else if(tipo_ingreso == 'Producto' && !bar_code){
+        
+        if(!bar_code){
 
             alert('Es necesario buscar un producto')
             elements['bar_code'].focus()
             buttons['enviar'].attr('disabled', false)
 
+        } else if(comission !== 0 && !comission)  {
 
-        }else if(tipo_ingreso === 'Servicio' && !price){
-
-            alert('Es necesario llenar el campo Precio')
-            elements['price'].focus()
-            buttons['enviar'].attr('disabled', false)
-
-        }else if(tipo_ingreso === 'Servicio' && !name) {
-
-            alert('Es necesario llenar el campo Nombre')
-            elements['name'].focus()
-            buttons['enviar'].attr('disabled', false)
-        
-        } else if(!comission) {
-
-            alert('Es necesario llenar el campo Procentaje Vendedor')
+            alert('El porcentaje de comision tiene que ser un número')
             elements['comission'].focus()
+            elements['comission'].val()
             buttons['enviar'].attr('disabled', false)
 
-        } else {
-            $.post('/venta/', {
+        }else {
+            $.post('/venta/producto/', {
                 el_name: name,
                 is_card: is_card,
-                value: amount * price,
-                description: description,
+                value: price,
                 owner_document: owner_document,
                 client_document: client_document,
+                product_code: bar_code,
                 percent: comission,
             }, function(data) {
 
@@ -155,13 +97,12 @@ $(document).ready(function() {
         }
         e.preventDefault()
     }) 
-
-
 })
 
 function to_int($element) {
     var value = $element.val()
     value = value == '' ? 0 : parseInt(value)
+    if(value < 0) return NaN
     return value
 }
 
@@ -169,35 +110,13 @@ function trimText($element) {
     return $.trim($element.val())
 }
 
-function saleProduct(elements, buttons) {
-    elements['price'].attr('readonly', true)
-    elements['name'].attr('readonly', true)
-    elements['name'].attr('placeholder', 'Nombre del producto')
-    elements['bar_code'].attr('disabled', false)
-    elements['amount_holder'].val('')
-    buttons['search_product'].attr('disabled', false)
-}
-
-function saleService(elements, buttons) {
-    elements['price'].attr('readonly', false)
-    elements['name'].attr('readonly', false)
-    elements['name'].attr('placeholder', 'Nombre del servicio')
-    elements['bar_code'].attr('disabled', true)
-    elements['amount_holder'].val('100')
-    buttons['search_product'].attr('disabled', true)
-}
-
 function initialize(elements) {
 
     elements['name'].val('')
     elements['price'].val('')
-    elements['amount'].val('')
     elements['bar_code'].val('')
     elements['comission'].val('')
-    elements['descripcion'].val('')
-    elements['amount_holder'].val('')
     elements['cedula_cliente'].val('')
-    elements['cedula_vendedor'].val('')
 
     elements['pay_with_card'].prop('checked', false)
 
---
+++ sales/static/js/service.js
@@ -0,0 +1,103 @@
+$(document).ready(function() {
+
+    elements = {
+        'price':            $('#price'),
+        'comission':        $('#comission'),
+        'descripcion':      $('#descripcion'),
+        'pay_with_card':    $('#pay_with_card'),
+        'cedula_cliente':   $('#cedula_cliente'),
+        'cedula_vendedor':  $('#cedula_vendedor')
+    }
+
+    buttons = {
+        'enviar':           $('#enviar')
+    }
+
+    forms = {
+        'register':     $('#register'),
+    }
+
+    initialize(elements)
+
+    forms['register'].on('submit', function(e) {
+
+        buttons['enviar'].attr('disabled', true)
+
+        var price           =   to_int(elements['price']),
+            is_card         =   elements['pay_with_card'].prop('checked'),
+            comission       =   to_int(elements['comission']),
+            description     =   trimText(elements['descripcion']),
+            owner_document  =   trimText(elements['cedula_vendedor']),
+            client_document =   trimText(elements['cedula_cliente'])
+
+        if(!description) {
+
+            alert('Tienes que ingresar una descripción')
+            elements['descripcion'].val('')
+            buttons['enviar'].attr('disabled', false)
+        
+        } else if(!price) {
+
+            alert('El precio tiene que ser mayor a cero.')
+            elements['price'].val('')
+            elements['price'].focus()
+            buttons['enviar'].attr('disabled', false)
+
+        } else if(comission !== 0 && !comission) {
+
+            alert('El porcentaje del vendedor tiene que ser un número.')
+            elements['comission'].val('')
+            elements['comission'].focus()            
+            buttons['enviar'].attr('disabled', false)
+
+        }else {
+
+            $.post('venta/servicio', {
+                el_name: description,
+                is_card: is_card,
+                value:  price,
+                owner_document: owner_document,
+                client_document: client_document,
+                percent: comission,
+            }, function(data) {
+
+                if(data.ok) {
+                    alert('El ingreso se registro correctamente.')
+                    initialize(elements)
+                    
+                } else {
+                    alert(data.msg)
+                }
+
+                buttons['enviar'].attr('disabled', false)
+
+            })
+            
+        }
+        e.preventDefault()
+    }) 
+
+
+})
+
+function to_int($element) {
+    var value = $element.val()
+    value = value == '' ? 0 : parseInt(value)
+    if (value < 0) return NaN
+    return value
+}
+
+function trimText($element) {
+    return $.trim($element.val())
+}
+
+function initialize(elements) {
+
+    elements['price'].val('')
+    elements['comission'].val('')
+    elements['descripcion'].val('')
+    elements['cedula_cliente'].val('')
+
+    elements['pay_with_card'].prop('checked', false)
+
+}
